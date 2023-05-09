04fb18fdc2b3807bf7ef17b9f2edb5baa288c1b1
火子 <yxyong@126.com>
2023-05-09 17:56:25 +0800

    判断检查有误

--- src/OAuthServerMiddleware.php
+++ src/OAuthServerMiddleware.php
@@ -25,7 +25,7 @@ class OAuthServerMiddleware implements MiddlewareInterface
    */
   public function __construct(array $config)
   {
-    if (!isset($config['storage']) && !isset($config['storage']['database'])) throw new \Exception('存储服务器未配置！');
+    if (!isset($config['storage']) || !isset($config['storage']['database'])) throw new \Exception('存储服务器未配置！');
     if ($config['storage']['type'] == 'mysql') $this->storage = new Database($config['storage']['database']);
     else  $this->storage = new Client($config['storage']['database']['parameters'], $config['storage']['database']['options']);
     //授权服务器分发的公钥
